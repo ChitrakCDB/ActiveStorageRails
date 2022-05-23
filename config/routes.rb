ef02b76e3b957a9166b16969cf43d8222cad53f3
chitrak-bhatt <chitrak.b@simformsolutions.com>
2022-05-23 18:09:24 +0530

    Add active storage practical

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :posts do
+    member do
+      delete :delete_file
+    end
+  end
+  root 'posts#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
